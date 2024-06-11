f0ae6d1064fbb05e9605bb4018b9ea5ceca6e01b
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2024-06-11 16:30:46 +0530

    update get context method

--- src/utils/agent_utils.py
+++ src/utils/agent_utils.py
@@ -1,6 +1,7 @@
 from crewai import Agent, Task
 from langchain_community.llms import LlamaCpp
 import yaml
+import os
 
 
 def create_agent_from_yaml(agent_yaml_file: str, llm: LlamaCpp):
@@ -19,7 +20,7 @@ def create_agent_from_yaml(agent_yaml_file: str, llm: LlamaCpp):
     return agent
 
 
-def create_task_from_yaml(task_yaml_file: str, agent: Agent,context:str):
+def create_task_from_yaml(task_yaml_file: str, agent: Agent, context: str):
     with open(task_yaml_file, 'r') as file:
         config = yaml.safe_load(file)
 
@@ -31,3 +32,10 @@ def create_task_from_yaml(task_yaml_file: str, agent: Agent,context:str):
     )
 
     return task
+
+
+def get_context(file_path):
+    if file_path and os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return file.read()
+    return "Generate a story"  # Default prompt if the file is not found
